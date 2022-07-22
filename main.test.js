396046a3cd07fbd5f0bd7d1f046de226cc472128
lin <weiyandesign@gmail.com>
2022-07-22 13:39:49 +0800

    feat(parseObjToNumberArr): add config

--- main.test.js
+++ main.test.js
@@ -1,31 +1,5 @@
-import { parseObjToNumber } from "./main.js";
-
-test.each(
-    parseObjToNumber({
-        apple: 1,
-        banana: {
-            a: 2,
-            b: [{ c: 3, d: 4 }],
-        },
-        cat: {
-            e: 5,
-            f: [6, 7],
-            g: { j: "test", h: 8, i: 9 },
-            k: false,
-        },
-        dog: true,
-        eat: {
-            p: {
-                q: {
-                    r: {
-                        s: 10,
-                        t: [11, 12],
-                    },
-                },
-                u: "v",
-            },
-        },
-    })
-)("Parse object to number array", (num) => {
+import { parseObjToNumber } from './main.js';
+import { obj } from './main.config.js';
+test.each(parseObjToNumber(obj))('Parse object to number array', num => {
     expect(num).toEqual(expect.any(Number));
 });
