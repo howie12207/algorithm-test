f42f7e260fc77115df1bc48b3da8146c1d3749e2
lin <weiyandesign@gmail.com>
2022-07-29 11:25:24 +0800

    test: happy number

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-import { obj } from './main.config.js';
 /**
- * 請將物件展開回傳包含所有數字的陣列
- * @return {number[]}
+ * 編寫一個算法來確定一個數字 n 是否為快樂數字。
+ * 快樂數字是由以下過程定義的數字：
+ * 從任何正整數開始，將數字替換為其數字的平方和。
+ * 重複這個過程，直到數字等於 1（它將保持不變），或者它在不包括 1的循環中無限循環。
+ * 這個過程以 1 結束的那些數字是快樂的。
+ * 如果是快樂數字回傳 true 否則回 false。
+ *
+ * n 的範圍是 1 <= n <= 2^31 - 1
+ *
+ * Eample n = 19
+ * Output: true
+ * Explanation:
+ * 1^2 + 9^2 = 82
+ * 8^2 + 2^2 = 68
+ * 6^2 + 8^2 = 100
+ * 1^2 + 0^2 + 0^2 = 1
+ *
+ * Eample n = 2
+ * Output: false
+ *
+ * @param {number} n
+ * @return {boolean}
  */
-export const parseObjToNumber = data => {};
 
-parseObjToNumber(obj);
+export const isHappy = n => {};
--- main.test.js
+++ main.test.js
@@ -1,5 +1,14 @@
-import { parseObjToNumber } from './main.js';
-import { obj } from './main.config.js';
-test.each(parseObjToNumber(obj))('Parse object to number array', num => {
-    expect(num).toEqual(expect.any(Number));
+import { isHappy } from './main.js';
+
+test('Is 2 a happy number?', () => {
+    expect(isHappy(2)).toBe(false);
+});
+test('Is 19 a happy number?', () => {
+    expect(isHappy(19)).toBe(true);
+});
+test('Is 1934400033914900032 a happy number?', () => {
+    expect(isHappy(1934400033914900032)).toBe(true);
+});
+test('Is 19000389098914900032 a happy number?', () => {
+    expect(isHappy(19000389098914900032)).toBe(false);
 });
